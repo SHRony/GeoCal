a6e5fa790785285254d9ae0165aad85d286232a0
Raiden <shrony101@gmail.com>
2021-06-16 14:52:46 +0600

    Update Graph.java

--- geocal/Graph.java
+++ geocal/Graph.java
@@ -45,40 +45,11 @@ public class Graph {
      * 
      * @param layout of primary stage
      */
-    public static void Own_graph(GeoPane layout){
-        
-        Stage window = new Stage();
-        //window.initStyle(StageStyle.UTILITY);
-        window.resizableProperty().setValue(Boolean.FALSE);
-        window.setTitle("Enter your function");
-
-        Label txt = new Label("f(x) = ");
-        Label txt2 = new Label("");
-        TextField function = new TextField();
-        function.setText("Cos(Tan( x ))");
-        Button submit = new Button("Submit");
-        Label example = new Label();
-        example.setText("Example:\nf(x)=(Sin(x^2))^2\nf(x)=Sin(Cos(Tan(x^2)))\nf(x)=-5*x^2-3*x/2.0+x%10\nf(x)=Cosec(x)+sec(2*x)+cot(x)-exp(x/10)\nf(x)=10\nf(x)=Log10(x+5)+Ln(x)-log2(x)\nf(x)=abs(x-5)-sqrt(x)");
-        example.setTextFill(Color.RED);
-
-        GridPane root = new GridPane();
-        root.addRow(1, txt, function, submit);
-        //root.addRow(3, txt2,example);
-        Scene scene = new Scene(root, 500,200);
-        window.setScene(scene);
-        window.show();
-
-
-        submit.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent ae) {
-                
-                String fun = function.getText();
-
+    public static void Own_graph(GeoPane layout,String fun){
                 if(fun==null || fun.isEmpty())
                 {
                     Alert.display("Error..!", "Please enter your function");
+                    
                 }
                 else
                 {
@@ -102,224 +73,15 @@ public class Graph {
                         Line line = MyLine(prex, prey, x, y);
                         prex=x; prey=y;
                         layout.chld.getChildren().addAll(line);
+                        GraphMenu.currentFunctions.add(line);
             
                     }
                     
-                    window.close();
                 }
-            }
-        });
 
 
     }
     
-    public static void Sin(GeoPane pane)
-    {
-        Graph g = new Graph();
-        for(int x =-7500; x<=7500; x+=1)
-        {
-            g.points.add(x+0.0);
-            double y=ZOOM*Math.sin(x/ZOOM);
-            g.points.add(y);
-        }
-        pane.chld.getChildren().addAll(g.polyline);
-    }
-    
-    public static void Cos(GeoPane pane)
-    {
-        Graph g = new Graph();
-        for(int x =-7500; x<=7500; x+=1)
-        {
-            g.points.add(x+0.0);
-            double y=ZOOM*Math.cos(x/ZOOM);
-            g.points.add(y);
-        }
-        pane.chld.getChildren().addAll(g.polyline);
-    }
-    
-    public static void Tan(GeoPane pane)
-    {
-        double prex=-7499,prey=ZOOM*Math.tan(-7499.0/ZOOM);
-        for(int x =-7500; x<=7500; x+=1)
-        {
-            double y=ZOOM*Math.tan(x/ZOOM);
-            if(diffSign(y, prey)){
-                prex=x; prey=y;
-                continue;
-            }
-            
-            Line line = MyLine(prex, prey, x, y);
-            prex=x; prey=y;
-            pane.chld.getChildren().addAll(line);
-        }
-    }
-    
-    public static void Cot(GeoPane pane)
-    {
-        double prex=-7499,prey=ZOOM*(1.0/Math.tan(-7499.0/ZOOM));
-        for(int x =-7500; x<=7500; x+=1)
-        {
-            if(Math.tan(x/ZOOM)==0.0)
-            {
-                continue;
-            }
-            double y=ZOOM*(1.0/Math.tan(x/ZOOM));
-
-            if(diffSign(y, prey)){
-                prex=x; prey=y;
-                continue;
-            }
-            
-            Line line = MyLine(prex, prey, x, y);
-            prex=x; prey=y;
-            pane.chld.getChildren().addAll(line);
-            
-        }
-    }
-    public static void Sec(GeoPane pane)
-    {
-        double prex=-7499,prey=ZOOM*(1.0/Math.cos(-7499.0/ZOOM));
-        for(int x =-7500; x<=7500; x+=1)
-        {
-            if(Math.cos(x/ZOOM)==0.0)
-            {
-                continue;
-            }
-            double y=ZOOM*(1.0/Math.cos(x/ZOOM));
-
-            if(diffSign(y, prey)){
-                prex=x; prey=y;
-                continue;
-            }
-            
-            Line line = MyLine(prex, prey, x, y);
-            prex=x; prey=y;
-            pane.chld.getChildren().addAll(line);
-            
-        }
-    }
-    
-    public static void Cosec(GeoPane pane)
-    {
-        double prex=-7499,prey=ZOOM*(1.0/Math.sin(-7499.0/ZOOM));
-        for(int x =-7500; x<=7500; x+=1)
-        {
-            if(Math.sin(x/ZOOM)==0.0)
-            {
-                continue;
-            }
-            double y=ZOOM*(1.0/Math.sin(x/ZOOM));
-
-            if(diffSign(y, prey)){
-                prex=x; prey=y;
-                continue;
-            }
-            
-            Line line = MyLine(prex, prey, x, y);
-            prex=x; prey=y;
-            pane.chld.getChildren().addAll(line);
-            
-        }
-    }
-    
-    public static void Square(GeoPane pane)
-    {
-        Graph g = new Graph();
-        for(int x =-7500; x<=7500; x+=1)
-        {
-            g.points.add(x+0.0);
-            double y=x*x/ZOOM;
-            g.points.add(y);
-        }
-        pane.chld.getChildren().addAll(g.polyline);
-    }
-    
-    public static void Sqrt(GeoPane pane)
-    {
-        Graph g = new Graph();
-        final double ZOOM = 30.0;
-        for(int x =0; x<=7500; x+=1)
-        {
-            g.points.add(x+0.0);
-            double y=ZOOM*Math.sqrt(x/ZOOM);
-            g.points.add(y);
-        }
-        pane.chld.getChildren().addAll(g.polyline);
-    }
-    public static void Ln(GeoPane pane)
-    {
-        Graph g = new Graph();
-        for(int x =1; x<=7500; x+=1)
-        {
-            g.points.add(x+0.0);
-            double y=ZOOM*Math.log(x);
-            g.points.add(y);
-            //System.out.println(x+"..."+y);
-        }
-        pane.chld.getChildren().addAll(g.polyline);
-    }
-    
-    public static void Log10(GeoPane pane)
-    {
-        Graph g = new Graph();
-        for(int x =1; x<=7500; x+=1)
-        {
-            g.points.add(x+0.0);
-            double y=ZOOM*Math.log10(x);
-            g.points.add(y);
-            //System.out.println(x+"..."+y);
-        }
-        pane.chld.getChildren().addAll(g.polyline);
-    }
-    
-    public static void Log2(GeoPane pane)
-    {
-        Graph g = new Graph();
-        for(int x =1; x<=7500; x+=1)
-        {
-            g.points.add(x+0.0);
-            double y=ZOOM*Math.log10(x)/Math.log10(2.0);
-            g.points.add(y);
-            //System.out.println(x+"..."+y);
-        }
-        pane.chld.getChildren().addAll(g.polyline);
-    }
-    
-    public static void Abs(GeoPane pane)
-    {
-        Graph g = new Graph();
-        for(int x =-7500; x<7500; x+=1)
-        {
-            g.points.add(x+0.0);
-            double y=ZOOM*Math.abs(x/ZOOM);
-            g.points.add(y);
-        }
-        pane.chld.getChildren().addAll(g.polyline);
-    }
-    
-    public static void Ex(GeoPane pane)
-    {
-        Graph g = new Graph();
-        for(int x =-1500; x<=500; x++)
-        {
-            g.points.add(x+0.0);
-            double y=ZOOM*Math.exp(x/ZOOM);
-            g.points.add(y);
-        }
-        pane.chld.getChildren().addAll(g.polyline);
-    }
-    
-    public static void Cubic(GeoPane pane)
-    {
-        Graph g = new Graph();
-        for(int x =-7500; x<=7500; x++)
-        {
-            g.points.add(x+0.0);
-            double y=ZOOM*Math.pow(x/ZOOM, 3.0);
-            g.points.add(y);
-        }
-        pane.chld.getChildren().addAll(g.polyline);
-    }
     
     public static boolean diffSign(double x, double y)
     {
